e97c557e141093bf58af8dd044bb543187bc396d
Alex41 <vk.alex41@gmail.com>
2024-01-02 20:54:12 +0200

    another: repackage

---
+++ delivery/depth_service.go
@@ -0,0 +1,9 @@
+package delivery
+
+import "github.com/ward-cap/go-binance/common"
+
+// Ask is a type alias for PriceLevel.
+type Ask = common.PriceLevel
+
+// Bid is a type alias for PriceLevel.
+type Bid = common.PriceLevel
