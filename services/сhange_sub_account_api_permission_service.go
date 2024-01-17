a961ffee865394e4c7164903f2c8dadc140190c7
Alex41 <vk.alex41@gmail.com>
2024-01-17 12:15:17 +0200

    feat: add ChangeSubAccountApiPermissionService

---
+++ services/сhange_sub_account_api_permission_service.go
@@ -0,0 +1,63 @@
+package binance
+
+import (
+	"context"
+	"net/http"
+)
+
+func (c *Client) NewChangeSubAccountApiPermissionService() *SubAccountDeleteApiKeyService {
+	return &SubAccountDeleteApiKeyService{c: c}
+}
+
+type ChangeSubAccountApiPermissionService struct {
+	c                *Client
+	subAccountID     string
+	subAccountApiKey string
+	canTrade         bool
+	futuresTrade     bool
+	marginTrade      bool
+}
+
+func (s *ChangeSubAccountApiPermissionService) SubAccountID(subAccountID string) *ChangeSubAccountApiPermissionService {
+	s.subAccountID = subAccountID
+	return s
+}
+
+func (s *ChangeSubAccountApiPermissionService) CanTrade(canTrade bool) *ChangeSubAccountApiPermissionService {
+	s.canTrade = canTrade
+	return s
+}
+
+func (s *ChangeSubAccountApiPermissionService) FuturesTrade(futuresTrade bool) *ChangeSubAccountApiPermissionService {
+	s.futuresTrade = futuresTrade
+	return s
+}
+
+func (s *ChangeSubAccountApiPermissionService) MarginTrade(marginTrade bool) *ChangeSubAccountApiPermissionService {
+	s.marginTrade = marginTrade
+	return s
+}
+
+func (s *ChangeSubAccountApiPermissionService) SubAccountApiKey(subAccountApiKey string) *ChangeSubAccountApiPermissionService {
+	s.subAccountApiKey = subAccountApiKey
+	return s
+}
+
+func (s *ChangeSubAccountApiPermissionService) Do(ctx context.Context, opts ...RequestOption) (err error) {
+	r := &request{
+		method:   http.MethodDelete,
+		endpoint: "/sapi/v1/broker/subAccountApi/permission",
+		secType:  secTypeSigned,
+	}
+	m := params{
+		"subAccountId":     s.subAccountID,
+		"subAccountApiKey": s.subAccountApiKey,
+		"canTrade":         s.canTrade,
+		"futuresTrade":     s.futuresTrade,
+		"marginTrade":      s.marginTrade,
+	}
+	r.setParams(m)
+	_, err = s.c.callAPI(ctx, r, opts...)
+
+	return err
+}
