8079b588207db95b9b9ca3c0b66cab9ce3ddb14f
Alex41 <vk.alex41@gmail.com>
2024-01-26 17:28:45 +0200

    Refactor NewUpdateIPRestrictionSubAccountAPIKeyService() method

--- services/update_ip_restriction_sub_account_api_key.go
+++ services/update_ip_restriction_sub_account_api_key.go
@@ -6,53 +6,53 @@ import (
 	"strings"
 )
 
-func (c *Client) NewUpdateIPRestrictionSubAccountAPIKeyService() *UpdateIPRestrictionSubAccountAPIKeyService {
-	return &UpdateIPRestrictionSubAccountAPIKeyService{c: c}
+func (c *Client) NewUpdateIPRestrictionSubAccountAPIKeyService(
+	email, subAccountApiKey, status string,
+	ipAddress []string,
+) *UpdateIPRestrictionSubAccountAPIKeyService {
+	return &UpdateIPRestrictionSubAccountAPIKeyService{
+		c:                c,
+		email:            email,
+		subAccountApiKey: subAccountApiKey,
+		ipAddress:        ipAddress,
+		status:           status,
+	}
 }
 
 type UpdateIPRestrictionSubAccountAPIKeyService struct {
 	c                *Client
-	subAccountID     string
+	email            string
 	subAccountApiKey string
 	ipAddress        []string
 	status           string
 }
 
-func (s *UpdateIPRestrictionSubAccountAPIKeyService) SubAccountID(subAccountID string) *UpdateIPRestrictionSubAccountAPIKeyService {
-	s.subAccountID = subAccountID
-	return s
-}
-
-func (s *UpdateIPRestrictionSubAccountAPIKeyService) SetStatus(status string) *UpdateIPRestrictionSubAccountAPIKeyService {
-	s.status = status
-	return s
-}
-
-func (s *UpdateIPRestrictionSubAccountAPIKeyService) SetIP(ips []string) *UpdateIPRestrictionSubAccountAPIKeyService {
-	s.ipAddress = ips
-	return s
-}
-
-func (s *UpdateIPRestrictionSubAccountAPIKeyService) SubAccountApiKey(subAccountApiKey string) *UpdateIPRestrictionSubAccountAPIKeyService {
-	s.subAccountApiKey = subAccountApiKey
-	return s
-}
-
-func (s *UpdateIPRestrictionSubAccountAPIKeyService) Do(ctx context.Context, opts ...RequestOption) (err error) {
+func (s *UpdateIPRestrictionSubAccountAPIKeyService) Do(ctx context.Context, opts ...RequestOption) (def UpdateIPRestrictionSubAccountAPIKeyResponse, _ error) {
 	r := &request{
 		method:   http.MethodPost,
-		endpoint: "/sapi/v2/broker/subAccountApi/ipRestriction",
+		endpoint: "/sapi/v2/sub-account/subAccountApi/ipRestriction",
 		secType:  secTypeSigned,
 	}
 	m := params{
-		"subAccountId":     s.subAccountID,
+		"email":            s.email,
 		"subAccountApiKey": s.subAccountApiKey,
 
 		"status":    s.status,
 		"ipAddress": strings.Join(s.ipAddress, ","),
 	}
 	r.setParams(m)
-	_, err = s.c.callAPI(ctx, r, opts...)
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return def, err
+	}
+	err = json.Unmarshal(data, &def)
 
-	return err
+	return def, err
+}
+
+type UpdateIPRestrictionSubAccountAPIKeyResponse struct {
+	Status     string   `json:"status"`
+	IpList     []string `json:"ipList"`
+	UpdateTime int64    `json:"updateTime"`
+	ApiKey     string   `json:"apiKey"`
 }
